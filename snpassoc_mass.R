9949a14f67da6f9e7776a53ef84e35f53f8054b1
Chris Friedline <cfriedline@vcu.edu>
2015-02-18 13:49:02 -0500

    add R file outputs for snpassoc

---
+++ snpassoc_mass.R
@@ -0,0 +1,15 @@
+
+library(SNPassoc)
+
+d = read.table('pheno_gt_base_pca_snpassoc.txt', sep="\t", row.names=1, header=T)
+
+#subtract b/c those are the PCA axes
+snp_cols = 4:(ncol(d)-10)
+snp_data = setupSNP(d, colSNPs=snp_cols, sep="/")
+pca_cols = (ncol(d)-9):ncol(d)
+pca_data = d[,pca_cols]
+
+wg = WGassociation(Mass~1+pca_data$PC1+pca_data$PC2+pca_data$PC3+pca_data$PC4+pca_data$PC5+pca_data$PC6+pca_data$PC7+pca_data$PC8+pca_data$PC9+pca_data$PC10, data=snp_data, model="co")
+saveRDS(wg, "wg_mass_co.rds")
+stats = WGstats(wg)
+saveRDS(stats, "wgstats_mass.rds")

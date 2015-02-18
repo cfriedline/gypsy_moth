f3780ceeca84db0d6c4ca3b3539c06f8d470b489
Chris Friedline <cfriedline@vcu.edu>
2015-02-18 17:49:03 -0500

    finalize berg/coop methods/stats/etc

--- snpassoc_total_dev_time.R
+++ snpassoc_total_dev_time.R
@@ -9,7 +9,7 @@ snp_data = setupSNP(d, colSNPs=snp_cols, sep="/")
 pca_cols = (ncol(d)-9):ncol(d)
 pca_data = d[,pca_cols]
 
-wg = WGassociation(Total_Dev_Time~1+pca_data$PC1+pca_data$PC2+pca_data$PC3+pca_data$PC4+pca_data$PC5+pca_data$PC6+pca_data$PC7+pca_data$PC8+pca_data$PC9+pca_data$PC10, data=snp_data, model="co")
+wg = WGassociation(Total_Dev_Time~1+pca_data$PC1+pca_data$PC2+pca_data$PC3+pca_data$PC4+pca_data$PC5+pca_data$PC6+pca_data$PC7+pca_data$PC8+pca_data$PC9+pca_data$PC10, data=snp_data, model="co", genotypingRate=5)
 saveRDS(wg, "wg_total_dev_time_co.rds")
 stats = WGstats(wg)
 saveRDS(stats, "wgstats_total_dev_time.rds")

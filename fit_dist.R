e3373f34f4077f39728533b72993c635993805a1
Chris Friedline <cfriedline@vcu.edu>
2015-12-01 13:50:51 -0500

    update distribution fitting

--- fit_dist.R
+++ fit_dist.R
@@ -1,17 +1,21 @@
-rm(list=ls())
-files = c("mass_postrb.txt", "pd_postrb.txt", "tdt_postrb.txt")
-data = read.table(files[2])$V1
-descdist(data,boot=100)
-dists = list("gamma", "exp", "weibull")
-fits = list()
-for (i in 1:length(dists)) {
-    dist = dists[[i]]
-    if (dist == "weibull") {
-        f = fitdist(data, dist, lower=0, upper=1, start=0.1)
-    } else {
-        f = fitdist(data, dist)
+compute_fits = function (infiles) {
+    dists = c("gamma", "exp", "weibull")
+    for (i in 1:length(infiles)) {
+        print(infiles[i])
+        data = read.table(infiles[i])$V1
+        #descdist(data,boot=100)
+        fits = list()
+        for (j in 1:length(dists)) {
+            dist = dists[j]
+            if (dist == "weibull") {
+                f = fitdist(data, dist, lower=0, upper=1, start=0.1)
+            } else {
+                f = fitdist(data, dist)
+            }
+            fits[[dist]] = f
+        }
+        #print(fits)
+        #cdfcomp(fits, legendtext = names(fits))
+        print(gofstat(fits, fitnames=names(fits)))[['aic']]
     }
-    fits[[dist]] = f
 }
-cdfcomp(fits, legendtext = names(fits))
-print(gofstat(fits, fitnames=names(fits)))
